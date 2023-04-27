6a471793075304162d2b756e0cc9649e09a512a4
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2023-04-27 14:36:21 +0530

    provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:text_to_image_gen/Pages/splash_screen.dart';
+import 'package:text_to_image_gen/utils/my_states.dart';
 
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await SentryFlutter.init(
         (options) => options.dsn = 'https://725035c013bd4e7495b4fc7e472f6c5f@o4505074740953088.ingest.sentry.io/4505074742132736',
     appRunner: () => runApp(const MyApp()),
@@ -15,14 +18,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textSelectionTheme: TextSelectionThemeData(
-          cursorColor: Colors.deepPurple.shade400,
-        ),
-      ),
-      home: const SplashScreen(),
-    );
+    return MultiProvider(
+        providers: [
+        ChangeNotifierProvider(create: (context) => ThemeState()),
+    ],
+    builder: (context, _) {
+      final themeState = Provider.of<ThemeState>(context);
+      themeState.getTheme();
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: themeState.currentThemeMode,
+        theme: themeState.getLightTheme(),
+        darkTheme: themeState.getDarkTheme(),
+        home: const SplashScreen(),
+      );
+    });
   }
 }
